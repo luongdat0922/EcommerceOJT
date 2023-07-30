ff353c77d562541e444799aec365d17eede90fc8
Luong Thanh Dat <luongdat0922@gmail.com>
2023-07-30 14:09:33 +0700

    task5

--- FE/angular-ecommerce/src/app/services/product.service.ts
+++ FE/angular-ecommerce/src/app/services/product.service.ts
@@ -16,9 +16,21 @@ export class ProductService {
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
-    // we are doing the work now!
+    // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+
+    // need to build URL based on the keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpCLient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     )
