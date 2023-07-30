537ed77f0dc48adec7997efa7dd80679e89b121e
Luong Thanh Dat <luongdat0922@gmail.com>
2023-07-30 07:01:21 +0700

    task1

---
+++ BE/spring-boot-ecommerce/src/main/java/com/dlstore/springbootecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,13 @@
+package com.dlstore.springbootecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.dlstore.springbootecommerce.entity.ProductCategory;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product_category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
---
+++ BE/spring-boot-ecommerce/src/main/java/com/dlstore/springbootecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.dlstore.springbootecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.dlstore.springbootecommerce.entity.Product;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
